818248eb7f1e9b6899bf86ddefcc055a541b3441
Alma <alma@inventati.org>
2025-10-23 13:09:49 +0000

    Feliz aniversário zuka!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> O jogo, você perdeu.</h1>
+    return <h1> Feliz aniversário zuka, você me inspira a ser minha melhor versão a cada dia. Quero ouvir seus sonhos e pesadelos. Até eu entender sua divindade impermanente. </h1>
 }
 
 export default Home;
