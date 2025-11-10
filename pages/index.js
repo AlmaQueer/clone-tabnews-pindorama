73d729740448c65deae9cdaf9a110397804f2e74
Alma <alma@inventati.org>
2025-11-10 16:09:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1> Feliz aniversário, Zuka! Você me inspira a ser minha melhor versão a cada dia. Quero ouvir seus sonhos e pesadelos, até eu entender sua divindade impermanente. </h1>
+  return (
+    <h1>
+      {" "}
+      Feliz aniversário, Zuka! Você me inspira a ser minha melhor versão a cada
+      dia. Quero ouvir seus sonhos e pesadelos, até eu entender sua divindade
+      impermanente.{" "}
+    </h1>
+  );
 }
 
 export default Home;
