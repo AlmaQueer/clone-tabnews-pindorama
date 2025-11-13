d40cc7bb3363358e9cdd7e3afc037743540ad0fe
Alma <alma@inventati.org>
2025-11-13 00:56:04 +0000

    add Filosofia do Projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1> Texto neutro de teste </h1>;
+  return (
+    <h1>
+      {" "}
+      Uma comunidade de conhecimentos e serviços transcestralmente dedicada a
+      questionar a tecnologia dominante e transformação da sociedade. Este é um
+      espaço onde estudantes e trabalhadores se reencontram, e onde
+      desenvolvedores alienados podem se tornar pesquisadores críticos,
+      fomentando processos de aprendizado e transformação social.{" "}
+    </h1>
+  );
 }
 
 export default Home;
